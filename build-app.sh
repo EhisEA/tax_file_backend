5043e446bf2ffe4b717dc6668d9e97ca52f46798
Michael Osajeh <michaelcosj@proton.me>
2024-12-29 18:19:55 +0100

    move doc generation before route cache clear

--- build-app.sh
+++ build-app.sh
@@ -3,6 +3,9 @@
 # Build assets using NPM
 npm run build
 
+# generate docs
+php artisan scribe:generate
+
 # Clear cache
 php artisan optimize:clear
 
@@ -11,6 +14,3 @@ php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
-
-# generate docs
-php artisan scribe:generate
