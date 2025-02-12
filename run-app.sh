b446ef41ffd7cef114670c45ffa59c18a0827bd0
michael <michaelcosj@proton.me>
2025-02-12 02:48:11 +0100

    update run-app to setup stripe webhooks

--- run-app.sh
+++ run-app.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Run migrations, process the Nginx configuration template and start Nginx
 php artisan migrate --force &&
-php artisan db:seed --force --class=TaxDocumentKindSeeder &&
-php artisan db:seed --force --class=UserWithNotificationSeeder &&
+# php artisan db:seed --force --class=TaxDocumentKindSeeder &&
+# php artisan db:seed --force --class=UserWithNotificationSeeder &&
+php artisan cashier:webhook --url "https://taxfilebackend-production.up.railway.app/stripe/webhook" &&
 node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf &&
 (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
