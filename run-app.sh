817a75ed9301e0be5af3c8fa1224f3b0f86174e2
michael <michaelcosj@proton.me>
2025-02-12 02:54:56 +0100

    remove cashier webhooks from run app script

--- run-app.sh
+++ run-app.sh
@@ -3,6 +3,6 @@
 php artisan migrate --force &&
 # php artisan db:seed --force --class=TaxDocumentKindSeeder &&
 # php artisan db:seed --force --class=UserWithNotificationSeeder &&
-php artisan cashier:webhook --url "https://taxfilebackend-production.up.railway.app/stripe/webhook" &&
+# php artisan cashier:webhook --url "https://taxfilebackend-production.up.railway.app/stripe/webhook" &&
 node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf &&
 (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
